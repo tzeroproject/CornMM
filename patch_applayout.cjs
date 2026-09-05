cfbf3668c416e340d5fe0e6f03d110f7ba43e35a
tzeroproject <tzerobaby@gmail.com>
2026-09-05 17:28:57 +0630

    chore: remove obsolete patch and test scripts

--- patch_applayout.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/layout/AppLayout.tsx', 'utf8');
-content = content.replace(
-  'return (\n    <>\n            <div className="min-h-screen',
-  'return (\n    <>\n      <AgeGate />\n      <div className="min-h-screen'
-);
-fs.writeFileSync('src/components/layout/AppLayout.tsx', content);
-console.log("AppLayout patched with AgeGate");
