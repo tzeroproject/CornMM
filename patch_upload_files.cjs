7f82804aa19e9224ec957a4d8d8a571497c3943b
tzeroproject <tzerobaby@gmail.com>
2026-09-05 17:30:30 +0630

    chore: remove obsolete patch and test scripts

--- patch_upload_files.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/pages/UploadPage.tsx', 'utf8');
-content = content.replace('const result = await new Promise((resolve, reject) => {', 'const result = await new Promise<any>((resolve, reject) => {');
-fs.writeFileSync('src/pages/UploadPage.tsx', content);
-console.log("UploadPage files patched");
