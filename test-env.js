e5a65a0148797ffa9606c4931f0f05ab2c7ada44
tzeroproject <tzerobaby@gmail.com>
2026-09-05 17:31:34 +0630

    chore: remove obsolete patch and test scripts

--- test-env.js
+++
@@ -1,3 +0,0 @@
-console.log("URL:", process.env.VITE_SUPABASE_URL ? "SET" : "UNSET");
-console.log("ANON:", process.env.VITE_SUPABASE_ANON_KEY ? "SET" : "UNSET");
-console.log("SERVICE:", process.env.SUPABASE_SERVICE_ROLE_KEY ? "SET" : "UNSET");
