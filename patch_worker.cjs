37417ec60e06a68fd2b3e0652731c0aa25f0358e
tzeroproject <tzerobaby@gmail.com>
2026-09-05 17:31:19 +0630

    chore: remove obsolete patch and test scripts

--- patch_worker.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('worker/index.ts', 'utf8');
-content = content.replace('ASSETS: Fetcher;', 'ASSETS: any;');
-content = content.replace('ctx: ExecutionContext', 'ctx: any');
-fs.writeFileSync('worker/index.ts', content);
-console.log("Worker patched");
