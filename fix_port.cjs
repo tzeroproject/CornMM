41346c7c9ce5e0dfd807773534c3c5950cbe5412
tzeroproject <tzerobaby@gmail.com>
2026-09-09 15:08:42 +0630

    chore: remove obsolete port fix script

--- fix_port.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('server.ts', 'utf8');
-content = content.replace(/const PORT = Number\(process\.env\.PORT \|\| 3000\);/g, "const PORT = 3000;");
-fs.writeFileSync('server.ts', content);
-console.log("Hardcoded PORT to 3000");
